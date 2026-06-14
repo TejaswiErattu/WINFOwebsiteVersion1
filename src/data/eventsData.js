97d97923494c45d0dbaec742daad84363e9f40e6
TejaswiErattu <tjerattu@gmail.com>
2026-06-13 22:06:24 -0700

    added pictures to events page

--- src/data/eventsData.js
+++ src/data/eventsData.js
@@ -13,27 +13,27 @@ export const eventsData = {
     {
       label: 'whether you\'re looking for hackathon & design experience...',
       events: [
-        { name: 'Annual Hackathon', image: '' },
-        { name: 'TechXperience', image: '' },
-        { name: 'Fearless', image: '' },
+        { name: 'Annual Hackathon', image: '/images/events/event-10.jpg' },
+        { name: 'TechXperience', image: '/images/events/event-07.jpg' },
+        { name: 'Fearless', image: '/images/events/event-09.png' },
       ],
     },
     {
       label: 'fun with new friends...',
       events: [
-        { name: 'Vision Boards', image: '' },
-        { name: 'Frost & Frosting', image: '' },
-        { name: 'Paint & Sip', image: '' },
-        { name: 'Galentine\'s Floral Arrangement', image: '' },
+        { name: 'Vision Boards', image: '/images/events/event-11.png' },
+        { name: 'Frost & Frosting', image: '/images/events/event-06.png' },
+        { name: 'Paint & Sip', image: '/images/events/event-05.png' },
+        { name: 'Galentine\'s Floral Arrangement', image: '/images/events/event-04.png' },
       ],
     },
     {
       label: 'or professional insights...',
       events: [
-        { name: 'Microsoft Company Tour', image: '' },
-        { name: 'Adobe Company Tour', image: '' },
-        { name: 'Hot Cocoa Table Talk', image: '' },
-        { name: 'Decoding HCI', image: '' },
+        { name: 'Microsoft Company Tour', image: '/images/events/event-01.jpg' },
+        { name: 'Adobe Company Tour', image: '/images/events/event-03.jpg' },
+        { name: 'Hot Cocoa Table Talk', image: '/images/events/event-02.jpg' },
+        { name: 'Decoding HCI', image: '/images/events/event-08.jpg' },
       ],
     },
   ],
